8c2f5566defcc2c731b0c22d10ffc345e8c6f86c
Rafael Lossurdo <lossurdo@gmail.com>
2023-12-14 20:42:13 -0300

    feat: Nimbus LAF

--- src/main/java/com/jdf/swing/iface/LookAndFeel.java
+++ src/main/java/com/jdf/swing/iface/LookAndFeel.java
@@ -45,4 +45,20 @@ public class LookAndFeel {
         }
     }
 
+    /**
+     * Seta LAF Nimbus
+     */
+    public static void setNimbusLookAndFeel() {
+        try {
+            for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
